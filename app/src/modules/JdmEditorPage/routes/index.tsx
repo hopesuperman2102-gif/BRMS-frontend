e48b42c0b84ef0446eb14b08e937ad83594f6abb
shoban <intern-clp01@chola1.murugappa.com>
2026-03-30 14:16:16 +0530

    jdm editor page

--- app/src/modules/JdmEditorPage/routes/index.tsx
+++ app/src/modules/JdmEditorPage/routes/index.tsx
@@ -1,10 +1,10 @@
 import type { AppRoute } from '@/core/types/routeTypes';
-import JdmEditorWithSimulator from '@/modules/JdmEditorPage/components/JdmEditorWithSimulator';
+import JdmEditorPage from '../Page/JdmEditorPage';
 
 const jdmEditorRoutes: AppRoute[] = [
   {
     path: '/vertical/:vertical_Key/dashboard/hub/:project_key/rules/editor',
-    element: JdmEditorWithSimulator,
+    element: JdmEditorPage,
     layout: 'none',
     metadata: {
       title: 'JDM Editor',
